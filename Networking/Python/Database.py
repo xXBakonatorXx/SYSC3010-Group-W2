b84bf60e5b94f09d5481af5266e65dad1e06b124
zachporter1999 <zach.ld.porter@gmail.com>
2019-10-31 11:58:32 -0400

    removed incorrect json class. working to fix update method, left comments at bottom describing which methods work.

--- Networking/Python/Database.py
+++ Networking/Python/Database.py
@@ -59,11 +59,7 @@ class Sqlite3:
     def tables(self):
         """ (None) -> Tuple
 
-        selects entries that match condition from table
-        and returns a tuple of the data
-
-        >>> database.select("test", '''name=="john smith"''')
-        ("john smith", 25)
+        selects tables from database.
         """
         self.__cursor__.execute('''SELECT * FROM sqlite_master WHERE type=="table"''')
         self.__database__.commit()
@@ -83,13 +79,20 @@ class Sqlite3:
             self.__cursor__.execute('''DELETE FROM {} WHERE {}'''.format(tableName, condition))
         self.__database__.commit()
     
-    def update(self, update, condition):
+    def update(self, tableName, update, condition):
         """ (str, str) -> None
 
         updates an entry in a table
         
         this feature is experimental and not yet inplemented
         """
+        # select tables
+        tables = self.tables()        
+
+        for table in tables:
+            if tableName == table[1]:
+
+
         return 0
 
     def close(self):
@@ -98,103 +101,6 @@ class Sqlite3:
         closes the connection to the database
         """
         self.__database__.close()
-
-class JSON:
-    def __init__(self, filename):
-        self.__filename__ = filename
-        self.__data__     = None
-        self.read()
-
-    def newTable(self, tablename):
-        """ (str) -> None 
-        
-        adds a new table to the database
-
-        >>> database.newTable('table')
-        """
-
-
-
-        if tablename in self.__data__.keys():
-            print("table already exists")
-            return False
-
-        self.__data__[tablename] = list()
-        self.__data__[tablename].append(dict())        
-        return True
-
-    def removeTable(self, tablename):
-        """ (str) -> None 
-        
-        removes a table to the database
-
-        >>> database.removeTable('table')
-        """
-
-        if tablename in self.__data__.keys():
-            return self.__data__.pop(tablename)
-        
-        print("Table {} does not exist".format(tablename))
-        return None
-
-    def insert(self, tablename, values):
-        """ (str, list) -> None
-
-        adds and entry into a table from a dctionary
-
-        >>> database.insert("table", [val, val, val])
-        """
-        exist = False
-        table = self.__data__[tablename]
-
-        for item in table:
-            if item["name"] == values[0]:
-                exist = True
-                keys = list(item.keys())
-                if len(values) == len(keys):
-                    for idx in range(len(keys)):
-                        item[keys[idx]] = values[idx]
-                else:
-                    print("invalid number of values")
-
-        if not exist:
-            table.append(dict())
-            keys = list(table[0].keys())
-
-            if len(values) == len(keys):
-                for idx in range(len(keys)):
-                    table[-1][keys[idx]] = values[idx]
-            else:
-                print("invalid number of values")
-
-    def remove(self, tablename, value, result):
-        table = self.__data__[tablename]
-        remove_items = []
-        for item in table:
-            if item[value] == result:
-                remove_items.append(item)
-        
-        for item in remove_items:
-            table.remove(item)
-
-    def read(self):
-        file = open(self.__filename__, 'r')
-        self.__data__ = json.load(file)
-        file.close()
-
-    def write(self):
-        file = open(self.__filename__, "w")
-        json.dump(self.__data__, file, indent=4)
-        file.close()
-
-    def encode(self):
-        return json.dumps(self.__data__)
-
-    def decode(self, datas):
-        self.data = json.loads(datas)
-
-    def getData(self):
-        return self.__data__
 
 def __testSQL__():
     db = Sqlite3("test.db")
@@ -215,13 +121,17 @@ def __testSQL__():
     db.removeTable("test")
     db.close()
 
-def __testJSON__():
-    db = JSON("test.json")
-    db.removeTable("test")
-    db.newTable("test")
-    db.remove("branches","name", "zach")
-    db.write()
-
 if __name__ == "__main__":
-    __testJSON__()
-    #__testSQL__()
+    __testSQL__()
+
+### TESTED ###
+# + newTable
+# + insert
+# + select
+# + tables
+# + removeTable
+# + close
+#
+### UNTESTED ###
+# - update
+# - delete
