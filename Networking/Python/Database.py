b8a9f26da4656bb519ed36db9aa809e7172ebb73
zachporter1999 <zach.ld.porter@gmail.com>
2019-11-01 13:27:18 -0400

    Fixed all methods for sqlite3 database class

--- Networking/Python/Database.py
+++ Networking/Python/Database.py
@@ -16,8 +16,8 @@ class Sqlite3:
         """
         try:
             self.__cursor__.execute('''CREATE TABLE {} ({})'''.format(tableName, dataVars))
-        except:
-            print("table {} already exists".format(tableName))
+        except sqlite3.OperationalError as err:
+            print(err)
         self.__database__.commit()
 
     def removeTable(self, tableName):
@@ -27,8 +27,11 @@ class Sqlite3:
 
         >>> database.remove("test")
         """
-        self.__cursor__.execute('''DROP TABLE {}'''.format(tableName))
-        self.__database__.commit()
+        try:
+            self.__cursor__.execute('''DROP TABLE {}'''.format(tableName))
+            self.__database__.commit()
+        except sqlite3.OperationalError as err:
+            print(err)
 
     def insert(self, tableName, values):
         """ (str, str) -> None
@@ -37,8 +40,11 @@ class Sqlite3:
 
         >>> database.insert("test", '''"john smith", 25''')
         """
-        self.__cursor__.execute('''INSERT INTO {} VALUES({})'''.format(tableName, values))
-        self.__database__.commit()
+        try:
+            self.__cursor__.execute('''INSERT INTO {} VALUES({})'''.format(tableName, values))
+            self.__database__.commit()
+        except sqlite3.OperationalError as err:
+            print(err)
 
     def select(self, tableName, condition=None):
         """ (str, str) -> Tuple
@@ -49,22 +55,29 @@ class Sqlite3:
         >>> database.select("test", '''name=="john smith"''')
         ("john smith", 25)
         """
-        if condition == None:
-            self.__cursor__.execute('''SELECT * FROM {}'''.format(tableName))
-        else:
-            self.__cursor__.execute('''SELECT * FROM {} WHERE {}'''.format(tableName, condition))
-        self.__database__.commit()
-        return self.__cursor__.fetchall()
+        try:
+            if condition == None:
+                self.__cursor__.execute('''SELECT * FROM {}'''.format(tableName))
+            else:
+                self.__cursor__.execute('''SELECT * FROM {} WHERE {}'''.format(tableName, condition))
+            self.__database__.commit()
+            return self.__cursor__.fetchall()
+        except sqlite3.OperationalError as err:
+            print(err)
+            return []
 
     def tables(self):
         """ (None) -> Tuple
 
         selects tables from database.
         """
-        self.__cursor__.execute('''SELECT * FROM sqlite_master WHERE type=="table"''')
-        self.__database__.commit()
-        return self.__cursor__.fetchall()
-
+        try:
+            self.__cursor__.execute('''SELECT * FROM sqlite_master WHERE type=="table"''')
+            self.__database__.commit()
+            return self.__cursor__.fetchall()
+        except sqlite3.OperationalError as err:
+            print(err)
+            return []
 
     def delete(self, tableName, condition=None):
         """ (str, str) -> None
@@ -73,27 +86,31 @@ class Sqlite3:
 
         >>> database.select("test", '''name=="john smith"''')
         """
-        if condition == None:
-            self.__cursor__.execute('''DELETE FROM {}'''.format(tableName))
-        else:
-            self.__cursor__.execute('''DELETE FROM {} WHERE {}'''.format(tableName, condition))
-        self.__database__.commit()
-    
-    def update(self, tableName, update, condition):
-        """ (str, str) -> None
+        try:
+            if condition == None:
+                self.__cursor__.execute('''DELETE FROM {}'''.format(tableName))
+            else:
+                self.__cursor__.execute('''DELETE FROM {} WHERE {}'''.format(tableName, condition))
+            self.__database__.commit()
+        except sqlite3.OperationalError as err:
+            print(err)
+
+    def update(self, tableName, updates, condition):
+        """ (str, tuple, str) -> None
 
         updates an entry in a table
-        
-        this feature is experimental and not yet inplemented
+        pass a tuple of updates to update multiple datas for one entry.
+
+
+        >>> database.update("test", ("name" = "jeff", "age" = 32), "name" = "john")
         """
-        # select tables
-        tables = self.tables()        
-
-        for table in tables:
-            if tableName == table[1]:
-
-
-        return 0
+        
+        for update in updates:
+            try:
+                self.__cursor__.execute('''UPDATE {} SET {} WHERE {}'''.format(tableName, update, condition))
+                self.__database__.commit()
+            except sqlite3.OperationalError as err:
+                print(err)
 
     def close(self):
         """ (None) -> None
@@ -106,10 +123,28 @@ def __testSQL__():
     db = Sqlite3("test.db")
     db.newTable("test", "name Text, value Integer")
     
-    db.insert("test", '''"d", 1''')
+    db.insert("test1", '''"d", 1''')
     db.insert("test", '''"e", 1''')
     db.insert("test", '''"f", 2''')
     db.insert("test", '''"f", 3''')
+
+    data = db.select("test")
+    print(data)
+
+    tables = db.tables()
+    for table in tables:
+        print(table)
+
+    db.update("test", ('''value=111''', '''name="eee"'''), '''name="e"''')
+
+    data = db.select("test")
+    print(data)
+
+    tables = db.tables()
+    for table in tables:
+        print(table)
+
+    db.delete("test")
 
     data = db.select("test")
     print(data)
@@ -130,8 +165,9 @@ if __name__ == "__main__":
 # + select
 # + tables
 # + removeTable
+# + update
 # + close
+# + delete
 #
 ### UNTESTED ###
-# - update
-# - delete
+#
