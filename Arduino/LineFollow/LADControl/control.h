b205166dea2d7dbd0552f62d11b40f5e145e43ec
zachporter1999 <zach.ld.porter@gmail.com>
2019-11-10 14:31:35 -0500

    Added obsticle avoidance using ultrasonic sensor

--- Arduino/LineFollow/LADControl/control.h
+++ Arduino/LineFollow/LADControl/control.h
@@ -1,2 +1,3 @@
-void control(double pid_output, double factor, double speed, double* motor_left, double* motor_right);
+void control_motors(double pid_output, double factor, double speed, double* motor_left, double* motor_right);
 double get_position(int values[], int n_sensors, double* weighted_sum, double* sum);
+bool detect_object(double threshold, double distance);
--- Arduino/LineFollow/LineFollow.ino
+++ Arduino/LineFollow/LineFollow.ino
@@ -1,16 +1,22 @@
 #include <control.h>
-
-#include <PID_v1.h>
 #include <AFMotor.h>
 
+// ultrasonic pins
+#define ULTRASONIC_TX 8
+#define ULTRASONIC_RX 7
+#define ULTRASONIC_THRESHOLD 10
+
+// ir pins
 #define IR_PIN 2
-#define N_SENSORS 5
+#define N_IR_SENSORS 5
+
+// line follow constants
 #define SPEED 75
 #define FACTOR 10
 #define CENTER 3
 
 // ir sensor values
-int ir_values[N_SENSORS];
+int ir_values[N_IR_SENSORS];
 
 // line position
 double w_sum = NULL;
@@ -19,37 +25,67 @@ double avg   = NULL;
 double motor_l = NULL;
 double motor_r = NULL;
 
+// obstacle
+double distance = 0;
+double obstacle = false;
+
 // config motors through motor shield
 AF_DCMotor motor1(1, MOTOR12_1KHZ);
 AF_DCMotor motor2(2, MOTOR12_1KHZ);
 
-// setup pid controller
-/*PID ladPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);*/
+void read_IR_sensors() {
+  // read ir sensors. 1 while no object, 0 while obeject (1 when line is underneath)
+  for (int sensor = 0; sensor < N_IR_SENSORS; sensor++) {
+    ir_values[sensor] = (double)digitalRead(sensor + IR_PIN);
+  }
+}
+
+double read_ultrasonic() {
+  digitalWrite(ULTRASONIC_TX, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(ULTRASONIC_TX, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC_TX, LOW);
+
+  return pulseIn(ULTRASONIC_RX, HIGH) * 0.034 / 2;
+}
 
 void setup() {
   Serial.begin(9600);
 
-  // the value pid adjusts towards
-  setpoint = 0;
-
-  for (int pin = IR_PIN; pin < IR_PIN + N_SENSORS; pin++) {
+  for (int pin = IR_PIN; pin < IR_PIN + N_IR_SENSORS; pin++) {
     pinMode(pin, INPUT);
   }
+
+  pinMode(ULTRASONIC_TX, OUTPUT);
+  pinMode(ULTRASONIC_RX, INPUT);
 }
 
 void loop() {
-  // read ir sensors. 1 while no object, 0 while obeject (1 when line is underneath)
-  for (int sensor = 0; sensor < N_SENSORS; sensor++) {
-    ir_values[sensor] = (double)digitalRead(sensor + IR_PIN);
+  //object avoidance
+  distance = read_ultrasonic();
+  obstacle = detect_object(ULTRASONIC_THRESHOLD, distance);
+  if (obstacle) {
+    motor_l = 0;
+    motor_r = 0;
+    Serial.println("OH shit an obsticle!!!");
+  } else {
+    
+    //Line Follow
+    read_IR_sensors();
+    avg = get_position(ir_values, N_IR_SENSORS, &w_sum, &sum);
+    control_motors(CENTER - avg, FACTOR, SPEED, &motor_l, &motor_r);
+    if (isnan(motor_l) || isnan(motor_r)) {
+      motor_l = -99;
+      motor_r = -99;
+      Serial.println("Recalculating...");
+    }
+    Serial.print(motor_l);
+    Serial.print("% ");
+    Serial.print(motor_r);
+    Serial.println("%");
   }
 
-  sum = 0;
-  w_sum = 0;
-  avg = get_position(ir_values, N_SENSORS, &w_sum, &sum);
-  
-  output = CENTER - avg;
-  control(output, FACTOR, SPEED, &motor_l, &motor_r);
-
-  Serial.print(motor_l);
-  Serial.println(motor_r);  
+  delay(500);
 }
