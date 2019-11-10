1de9a9abe651f8f5f24b8bb4f8a687c3f5e10609
zachporter1999 <zach.ld.porter@gmail.com>
2019-11-10 12:56:18 -0500

    code clean

--- Arduino/LineFollow/LineFollow.ino
+++ Arduino/LineFollow/LineFollow.ino
@@ -5,39 +5,19 @@
 
 #define IR_PIN 2
 #define N_SENSORS 5
-
-#define PIN0 2
-#define PIN1 3
-#define PIN2 4
-#define PIN3 5
-#define PIN4 6
-
 #define SPEED 75
 #define FACTOR 10
 #define CENTER 3
 
-// IR sensor values
-int val0  = 0;
-int val1  = 0;
-int val2  = 0;
-int val3  = 0;
-int val4  = 0;
-
+// ir sensor values
 int ir_values[N_SENSORS];
 
 // line position
 double w_sum = NULL;
 double sum   = NULL;
-double avg   = 0;
-double pos   = 0;
-double motor_l = 0;
-double motor_r = 0;
-
-//PID Params
-double setpoint;
-double input;
-double output;
-double Kp=1, Ki=0, Kd=0;
+double avg   = NULL;
+double motor_l = NULL;
+double motor_r = NULL;
 
 // config motors through motor shield
 AF_DCMotor motor1(1, MOTOR12_1KHZ);
@@ -55,17 +35,6 @@ void setup() {
   for (int pin = IR_PIN; pin < IR_PIN + N_SENSORS; pin++) {
     pinMode(pin, INPUT);
   }
-/*
-  //setup ir pins
-  pinMode(PIN0, INPUT);
-  pinMode(PIN1, INPUT);
-  pinMode(PIN2, INPUT);
-  pinMode(PIN3, INPUT);
-  pinMode(PIN4, INPUT);
-*/
-  //setup pid
-  /*ladPID.SetMode(AUTOMATIC);
-  ladPID.SetTunings(Kp, Ki, Kp);*/
 }
 
 void loop() {
@@ -73,40 +42,14 @@ void loop() {
   for (int sensor = 0; sensor < N_SENSORS; sensor++) {
     ir_values[sensor] = (double)digitalRead(sensor + IR_PIN);
   }
-/*
-  val0  = (int)digitalRead(PIN0);
-  val1  = (int)digitalRead(PIN1);
-  val2  = (int)digitalRead(PIN2);
-  val3  = (int)digitalRead(PIN3);
-  val4  = (int)digitalRead(PIN4);
-*/
 
-/*
-  // calculates position of line under the sensors
-  w_sum = (double)(val0 * 1.0 + val1 * 2.0 + val2 * 3.0 + val3 * 4.0 + val4 * 5.0);
-  sum   = (double)(val0 + val1 + val2 + val3 + val4);
-
-  if (sum == 0) {
-    sum = -4;
-  }
-  
-  avg   = (double)(w_sum / sum);
-*/
   sum = 0;
   w_sum = 0;
   avg = get_position(ir_values, N_SENSORS, &w_sum, &sum);
-  //pid work
   
   output = CENTER - avg;
   control(output, FACTOR, SPEED, &motor_l, &motor_r);
-  
-  /*ladPID.Compute();*/
 
   Serial.print(motor_l);
-  Serial.println(motor_r);
-
-  // drive wheels
-  
-
-  //delay(1000);
+  Serial.println(motor_r);  
 }
