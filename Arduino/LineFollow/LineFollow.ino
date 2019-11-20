f54f71031cb12f0c038d0d9c774b75b09d54f089
zachporter1999 <zach.ld.porter@gmail.com>
2019-11-19 21:31:54 -0500

    update imagecaprue.py

--- Arduino/LineFollow/LineFollow.ino
+++ Arduino/LineFollow/LineFollow.ino
@@ -2,18 +2,33 @@
 #include <AFMotor.h>
 
 // ultrasonic pins
+/*
+ * ULTRASONIC_THRESHOLD is the minimum distance at which 
+ * an object can be before it is detected as an obstacle
+ */
 #define ULTRASONIC_TX 8
 #define ULTRASONIC_RX 7
 #define ULTRASONIC_THRESHOLD 10
 
 // ir pins
+/*
+ * Assuming all ir sensors are wired next to one another
+ * IR_PIN is the values of the first pin, and N_IR_SENSORS
+ * is the number of ir sensors.
+ * 
+ * Allows for easy changing of the number of sensors.
+ */
 #define IR_PIN 2
 #define N_IR_SENSORS 5
 
 // line follow constants
-#define SPEED 75
-#define FACTOR 10
-#define CENTER 3
+/*
+ * CENTER is the position of the middle of the LAD Unit
+ * used for controling motor.
+ */
+#define MOTOR_SPEED 75
+#define STEERING_SENSITIVITY 10
+#define CENTER (1 + N_IR_SENSORS) / 2
 
 // ir sensor values
 int ir_values[N_IR_SENSORS];
@@ -21,25 +36,36 @@ int ir_values[N_IR_SENSORS];
 // line position
 double w_sum = NULL;
 double sum   = NULL;
-double avg   = NULL;
-double motor_l = NULL;
-double motor_r = NULL;
+double line_pos   = NULL;
+double mtr_spd_l = NULL;
+double mtr_spd_r = NULL;
 
 // obstacle
 double distance = 0;
 double obstacle = false;
 
 // config motors through motor shield
-AF_DCMotor motor1(1, MOTOR12_1KHZ);
-AF_DCMotor motor2(2, MOTOR12_1KHZ);
+AF_DCMotor motorLeft(1, MOTOR12_1KHZ);
+AF_DCMotor motorRight(2, MOTOR12_1KHZ);
 
+/*
+ * Reads the ir sensors.
+ * 
+ * reads from pin IR_PIN to IR_PIN + N_IR_SENSOrs
+ * sensor reads 0 when no line is detected
+ * sensor reads 1 when a line is detected
+ */
 void read_IR_sensors() {
-  // read ir sensors. 1 while no object, 0 while obeject (1 when line is underneath)
   for (int sensor = 0; sensor < N_IR_SENSORS; sensor++) {
     ir_values[sensor] = (double)digitalRead(sensor + IR_PIN);
   }
 }
 
+/*
+ * Reads ultrasonic sensor
+ * 
+ * returns distance to object.
+ */
 double read_ultrasonic() {
   digitalWrite(ULTRASONIC_TX, LOW);
   delayMicroseconds(2);
@@ -54,10 +80,12 @@ double read_ultrasonic() {
 void setup() {
   Serial.begin(9600);
 
+  // setup ir pins
   for (int pin = IR_PIN; pin < IR_PIN + N_IR_SENSORS; pin++) {
     pinMode(pin, INPUT);
   }
 
+  // setup ultrasonic pins
   pinMode(ULTRASONIC_TX, OUTPUT);
   pinMode(ULTRASONIC_RX, INPUT);
 }
@@ -66,24 +94,41 @@ void loop() {
   //object avoidance
   distance = read_ultrasonic();
   obstacle = detect_object(ULTRASONIC_THRESHOLD, distance);
+
+  // stop if there is an obstacle
   if (obstacle) {
-    motor_l = 0;
-    motor_r = 0;
+    mtr_spd_l = 0;
+    mtr_spd_r = 0;
     Serial.println("OH shit an obsticle!!!");
   } else {
     
     //Line Follow
     read_IR_sensors();
-    avg = get_position(ir_values, N_IR_SENSORS, &w_sum, &sum);
-    control_motors(CENTER - avg, FACTOR, SPEED, &motor_l, &motor_r);
-    if (isnan(motor_l) || isnan(motor_r)) {
-      motor_l = -99;
-      motor_r = -99;
+    line_pos = get_position(ir_values, N_IR_SENSORS, &w_sum, &sum);
+
+    // checks that line exists
+    if (line_pos != -eLadNoLine) {
+      // CENTER - line_pos shifts line position to be with respect to the center of the LAD Unit
+      control_motors(CENTER - line_pos, STEERING_SENSITIVITY, MOTOR_SPEED, &mtr_spd_l, &mtr_spd_r);
+    } else {
       Serial.println("Recalculating...");
     }
-    Serial.print(motor_l);
+
+    // drive motor
+    if (mtr_spd_l > 0 && mtr_spd_r > 0) {
+      motorLeft.setSpeed(mtr_spd_l);
+      motorRight.setSpeed(mtr_spd_r);
+  
+      motorLeft.run(FORWARD);
+      motorRight.run(FORWARD);
+    } else {
+      motorLeft.run(RELEASE);
+      motorRight.run(RELEASE);
+    }
+    
+    Serial.print(mtr_spd_l);
     Serial.print("% ");
-    Serial.print(motor_r);
+    Serial.print(mtr_spd_r);
     Serial.println("%");
   }
 
---
+++ ImageRecog/ImageCapture.py
@@ -0,0 +1,54 @@
+import os
+import subprocess
+import sys
+try:
+    import pyzbar.pyzbar as pyzbar
+    from PIL import Image
+except Exception as e:
+    print(type(e), e)
+
+class FsWebCamError(Exception):
+    def init(self, error):
+        print(error)
+
+class FsWebCam:
+    def __init__(self):
+        print("Starting camera...")
+
+    def capture(self, resolution, filename, banner=False):
+        banner_s = "--no-banner"
+        if banner:
+            banner_s = ""
+        try:
+            res = os.system('fswebcam -r {} {} {}'.format(resolution, banner_s, filename))
+            if res == 0:
+                raise FsWebCamError("fswebcam failed to run")
+            
+        except Exception as e:
+            print(type(e), e)
+
+    def __x_capture__(self, resolution, filename, banner=False):
+        banner_s = "--no-banner"
+        if banner:
+            banner_s = ""
+        try:
+            p = subprocess.Popen(['fswebcam', '-r', resolution, banner_s, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, err = p.communicate()
+            if err != b'':
+                raise FsWebCamError(err.decode('utf-8'))
+            
+        except Exception as e:
+            print(type(e), e)
+
+    def load(self, filename):
+        return Image.open(filename)
+
+    def decode(self, image):
+        return pyzbar.decode(image)
+
+if __name__ == "__main__":
+    cam = FsWebCam()
+    filename = "testFS.jpeg"
+    cam.capture("1280x720", filename, banner=False)
+    img = cam.load(filename)
+    cam.decode(img)
