0207168bc3261efc7abe2f0ab15bee5f69a81b82
denise-mayo <denise.e.mayo@gmail.com>
2019-11-29 10:29:57 -0500

    split mock into 2 separate files

--- sender.py
+++ sender.py
@@ -15,20 +15,7 @@ import select
 
 HOST = '127.0.0.1'
 
-def recieveData(port):
-    recPort = port
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.bind((HOST, recPort))
-        print("waiting for message")
-        while True:
-            data, addr = s.recvfrom(1024)
-            recieved = str(data)
-            print("recieved message:", recieved)
-            try:
-                while(data):
-                    #do nothing 
-            except timeout:
-                print("file downloaded")       
+    
 
 def fileTransfer(fileName):
     s =  socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
