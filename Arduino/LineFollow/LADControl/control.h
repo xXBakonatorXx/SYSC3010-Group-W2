8dad5a3a445b60e9d85bc0d8267196580229cd69
Erdem Yanikomeroglu <erdemyanikomer@gmail.com>
2019-11-30 14:20:49 -0400

    updated item fetch method

--- Arduino/LineFollow/LADControl/control.h
+++
@@ -1,3 +0,0 @@
-void control_motors(double pid_output, double factor, double speed, double* motor_left, double* motor_right);
-double get_position(int values[], int n_sensors, double* weighted_sum, double* sum);
-bool detect_object(double threshold, double distance);
