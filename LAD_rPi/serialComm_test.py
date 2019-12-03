6065f6e1d08790983c937bba36b546bd84aec719
zachporter1999 <zach.ld.porter@gmail.com>
2019-12-03 18:45:44 -0500

    addded path finding

--- LAD_rPi/serialComm_test.py
+++ LAD_rPi/serialComm_test.py
@@ -10,6 +10,7 @@ Project Group : W2
 
 """
 import ImageCapture
+import PathFinding
 import UDPSocket
 import keyboard
 import socket
@@ -23,14 +24,19 @@ class Database:
         self.data = dict()
         self.timestamp = 0
 
-    def search(self, table, entry_name):
+    def search(self, table, entry_val, byNumber=False):
         """ (str, str) -> dict
         
         Search the table for entry
         """
 
         for entry in self.data[table]:
-            if entry['name'] == entry_name:
+            if byNumber:
+                value = 'num'
+            else:
+                value = 'name'
+            
+            if entry[value] == entry_val:
                 return entry
         return None
 
@@ -42,14 +48,14 @@ OP_MANUAL       = 0x400 # 0100 0000 0000
 OP_AUTOMATIC    = 0x200 # 0010 0000 0000
 
 DRV_FORWARD     = 0x100 # 0001 0000 0000
-DRV_TURN_LEFT  = 0x080 # 0000 1000 0000
-DRV_TURN_RIGHT = 0x040 # 0000 0100 0000
+DRV_TURN_LEFT   = 0x080 # 0000 1000 0000
+DRV_TURN_RIGHT  = 0x040 # 0000 0100 0000
 MOV_ARM_UP      = 0x020 # 0000 0010 0000
 MOV_ARM_DOWN    = 0x010 # 0000 0001 0000
-MOV_WST_UP      = 0x008 # 0000 0000 1000
-MOV_WST_DOWN    = 0x004 # 0000 0000 0100
-MOV_HND_OPEN    = 0x002 # 0000 0000 0010
-MOV_HND_CLOSE   = 0x001 # 0000 0000 0001
+MOV_WRIST_UP    = 0x008 # 0000 0000 1000
+MOV_WRIST_DOWN  = 0x004 # 0000 0000 0100
+MOV_HAND_OPEN   = 0x002 # 0000 0000 0010
+MOV_HAND_CLOSE  = 0x001 # 0000 0000 0001
 
 LAD_X           = 0x100 # 0001 0000 0000
 LAD_XX          = 0x080 # 0000 1000 0000
@@ -63,9 +69,9 @@ LAD_MOV_ARM     = 0x002 # 0000 0000 0100
 LAD_NO_LINE     = 0x001 # 0000 0000 0010
 
 SERVER_ADDRESS = '127.0.0.1'
-UDP_PORT = 520
-UDP_ENCODING = 'utf-8'
-UDP_TIMEOUT = 1
+UDP_PORT       = 520
+UDP_ENCODING   = 'utf-8'
+UDP_TIMEOUT    = 1
 
 # files to store camera image in
 LOCATION_QR_FILE = 'locationQR.jpeg'
@@ -84,6 +90,13 @@ SERIAL_PORT = 'COM4'
 # and next location (next_location)
 last_location = 'home'
 next_location = None
+lad_location = 'home'
+
+def create_map(graph):
+    graph.addEdge(0, 1)
+    graph.addEdge(1, 2) 
+    graph.addEdge(1, 3) 
+    graph.addEdge(3, 4)  
 
 def get_turn(current_path, next_path, paths_list):
     """ (int, int, list of str) -> int
@@ -174,6 +187,12 @@ def decode_server(server_data):
     return database_dict
 
 
+def decode_arm_manual(arduino_op_code, up_code, down_code):
+    if (arduino_op_code & up_code):
+        return up_code
+    elif (arduino_op_code & down_code):
+        return down_code
+
 def decode_arduino(camera, database, arduino_op_code, next_location):
     """ (camera obj, dict, int, int, list, int) -> int
 
@@ -253,12 +272,26 @@ def decode_arduino(camera, database, arduino_op_code, next_location):
     elif (arduino_op_code & OP_MANUAL):
         # decode manual codes here.
 
-        """
-        ERDEM
-        CODE
-        HERE
+        #move forward
+        if (arduino_op_code & DRV_FORWARD):
+            return_code |= DRV_FORWARD
+
+            #turn left
+            if (arduino_op_code & DRV_TURN_LEFT):
+                return_code |= DRV_TURN_LEFT
+
+            #turn right
+            elif (arduino_op_code & DRV_TURN_RIGHT):
+                return_code |= DRV_TURN_RIGHT
+            
+            #go straight
+            elif (arduino_op_code & (DRV_TURN_LEFT & DRV_TURN_RIGHT)):
+                return_code |= DRV_TURN_LEFT | DRV_TURN_RIGHT
         
-        """
+        #move arm, wrist, hand
+        return_code |= decode_arm_manual(arduino_op_code, MOV_ARM_UP, MOV_ARM_DOWN)
+        return_code |= decode_arm_manual(arduino_op_code, MOV_WRIST_UP, MOV_WRIST_DOWN)
+        return_code |= decode_arm_manual(arduino_op_code, MOV_HAND_OPEN, MOV_HAND_CLOSE)
         
     return return_code
 
@@ -266,35 +299,60 @@ def encode_arduino(ser, op_code):
     print("encoding arduino command")
     ser.write(op_code)
 
+def find_path(graph, data):
+    item = database.search(ITEM_TABLE, data)
+    item_location = item['location']
+    final_location = database.search(LOCATION_TABLE, item_location)
+    final_location_idx = final_location['number']
+
+    current_location = database.search(LOCATION_TABLE, lad_location)
+    current_location_idx = current_location['number']
+
+    path_map.findAllPaths(current_location_idx, final_location_idx)
+    return path_map.getPath()
+
 
 # main funciton
 if __name__ == "__main__":
     #setup serial, udp client, camera, and database
+    path_map = PathFinding.Graph(5)
     ser = serial.Serial(port=SERIAL_PORT,baudrate='9600')
     client = UDPSocket.Client(SERVER_ADDRESS, UDP_PORT, UDP_ENCODING, UDP_TIMEOUT)
     cap = ImageCapture.FsWebCam()
     database = Database()
+
+    path2item = None
     
     res = OP_AUTOMATIC
+    updated_data = False
+
+    create_map(path_map)
 
     """ default values of server data, used to test diferent cases rn. will be over written by data read from server """
-    #server_data = "item_name"
+    server_data = "D_item"
     #server_data = str(OP_AUTOMATIC)
-    server_data = str(OP_MANUAL)
+    #server_data = str(OP_MANUAL)
     #server_data = '{"location": [{"name": "A", "pathA": "N"}, {"name": "B", "pathA": "N"}, {"name": "C", "pathA": "N"}, {"name": "D", "pathA": "N"}], "object": [{"name": "A", "location": "A"}, {"name": "B", "location": "B"}, {"name": "C", "location": "C"}, {"name": "D", "location": "D"}]}'
+
+    #debugg purpose
+    database.data = json.load(open('test.json'))
 
     time.sleep(2)
     print('ready')
 
     while (not keyboard.is_pressed('esc')):
         # read from data base
-        #server_data = client.receive()
 
         # read from server, if no new data after 1 sec the socket times out and server_data stays as its previous value.
-        server_data = client.receive()
+        #server_data = client.receive()
 
         # decode server data
-        data = decode_server(server_data)   
+        last_data = data
+        data = decode_server(server_data)
+
+        if data != last_data:
+            # flag that data has changed
+            updated_data = True
         
         if (type(data) == type(dict())):
             # update database cache
@@ -305,12 +363,16 @@ if __name__ == "__main__":
             print("referance database")
             #referance data base here
 
-            """
-                ERDEM 
-                CODE
-                HERE
-
-            """
+            if data != "cancel":
+                if updated_data:
+                    path2item = find_path(path_map, data)
+                    print(path2item)
+                else:
+                    # continue last command
+                    x = 0
+            else:
+                # return home
+                x = 0
 
         elif (type(data) == type(int())):
             # encode arduino command
