62f23df882c8ce245ed126d92ac7aae089e5558e
denise-mayo <denise.e.mayo@gmail.com>
2019-12-05 10:45:01 -0500

    more cleanup

--- DatabaseNetwork/decodeTest.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def json_to_dict(jsonfile):
-    with open("decode.txt", 'r') as f:
-        jsonDict = json.load(f) #load the file into our new dictionary
-        print(jsonDict)
-    
--- DatabaseNetwork/to_file.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def to_file(data):
-    with open("decoded.json", "w") as f:
-        json.dump(data, f)
